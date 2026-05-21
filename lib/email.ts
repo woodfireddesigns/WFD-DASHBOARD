7c2d433621011820ac05dee07aaa4d1ae61ad710
Michael Deschenes <michaeltdesh@gmail.com>
2026-05-21 10:43:27 -0400

    Revert TO back to woodfireddesigns.com

--- lib/email.ts
+++ lib/email.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const TO = "michaeltdesh@gmail.com";
+const TO = "michael@woodfireddesigns.com";
 const FROM = "WFD Notifications <notifications@woodfireddesigns.com>";
 
 const fmt = (n: number) => "$" + n.toLocaleString();
